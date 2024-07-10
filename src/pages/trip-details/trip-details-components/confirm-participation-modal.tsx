ece9e52ae11fa8e7a72e749c3df11389372c9ae2
brenocrepaldi <brenogaia2004@gmail.com>
2024-07-10 00:29:24 -0300

    refactors confirm participation and create link modal

--- src/pages/trip-details/trip-details-components/confirm-participation-modal.tsx
+++ src/pages/trip-details/trip-details-components/confirm-participation-modal.tsx
@@ -10,7 +10,7 @@ export function ConfirmParticipationModal({
 }: ConfirmParticipationModalProps) {
 	return (
 		<div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-			<div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+			<div className="w-[540px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
 				<div className="space-y-2">
 					<div className="flex items-center justify-between">
 						<h2 className="text-lg font-semibold">Confirmar participação</h2>
@@ -38,7 +38,7 @@ export function ConfirmParticipationModal({
 							<input
 								type="text"
 								placeholder="Seu nome completo"
-								className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+								className="bg-transparent placeholder-zinc-400 outline-none flex-1"
 							/>
 						</div>
 						<div className="h-14 px-4 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
@@ -47,7 +47,7 @@ export function ConfirmParticipationModal({
 								type="email"
 								name="email"
 								placeholder="Seu e-mail"
-								className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+								className="bg-transparent placeholder-zinc-400 outline-none flex-1"
 							/>
 						</div>
 					</div>
